b64f033addab479defdca8ca7fdeb01a29869487
MertensMing <13680543450@163.com>
2017-08-26 13:04:09 +0800

    update

--- config/index.js
+++ config/index.js
@@ -2,3 +2,17 @@ module.exports = function (env) {
   const config = require(`./${env}.js`);
   return config
 }
+function generatorEntry() {
+  var entry = {};
+
+  var jsRootPath = __dirname + '/js';
+  var files = glob.sync(path.join(jsRootPath, '**/main.+(js|jsx)'));
+  files.forEach(function(file) {
+      var fileBasePath = file.replace('/main.js', '');
+      var folderName = path.relative(jsRootPath, fileBasePath) || path.basename(fileBasePath);
+
+      entry[folderName] = file.replace(__dirname, '.').replace('.js', '').replace('.jsx', '');
+  });
+
+  return entry;
+}
